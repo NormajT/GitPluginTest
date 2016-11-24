4d2d53e0bfedb677bc72477618380f75380e5610
JamornT <JamornT@JAMORNT-DELL-XPS15>
2016-11-24 13:12:41 +0700

    1st time branch#1234

--- src/packt/jee/eclipse/git/GitTestApp.java
+++ src/packt/jee/eclipse/git/GitTestApp.java
@@ -2,5 +2,7 @@ package packt.jee.eclipse.git;
 
 public class GitTestApp {
 
-	//Remark
+	public static void main(String[] args) {
+		System.out.println("Hello Git, from branch bug#1234 !!");
+	}
 }
